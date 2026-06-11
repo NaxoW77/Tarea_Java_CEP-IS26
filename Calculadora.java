fdcd169e6c040594cf4f52c3104dc03ac3135288
crisviquez <cristopherviquezquiros@gmail.com>
2026-06-11 13:23:37 -0600

    Añadidas las funciones hiperbólicas

--- Calculadora.java
+++ Calculadora.java
@@ -13,7 +13,7 @@ import java.io.Console;
 public class Calculadora {
 
     // Función para mostrar errores y salir
-    public void mostrarError(String msg) {
+    public static void mostrarError(String msg) {
         System.out.println("Error: "+msg);
         System.exit(1);
         return;
@@ -22,22 +22,56 @@ public class Calculadora {
     // Funciones trigonométricas:
     
     // Función sin(x)
-    public double sinX(double x) {
+    public static double sinX(double x) {
         return Math.sin(x);
     }
 
     // Función cos(x)
-     public double cosX(double x) {
+     public static double cosX(double x) {
         return Math.cos(x);
     }
 
     // Función tan(x)
-     public double tanX(double x) {
+     public static double tanX(double x) {
         return Math.tan(x);
     }
 
+    // *Añadidas las funciones inversas* - Ronald M.
+
+    // Función arcsin(x)
+    public static double arcsinX(double x) {
+        return Math.asin(x);
+    }
+
+    // Función arccos(x)
+    public static double arccosX(double x) {
+        return Math.acos(x);
+    }
+
+    // Función arctan(x)
+    public static double arctanX(double x) {
+        return Math.atan(x);
+    }
+
+    // *Añadidas las funciones hiperbólicas* - Cristopher V.
+
+    // Función sinh(x)
+    public static double sinhX(double x) {
+        return Math.sinh(x);
+    }
+
+    // Función cosh(x)
+    public static double coshX(double x) {
+        return Math.cosh(x);
+    }
+
+    // Función tanh(x)
+    public static double tanhX(double x) {
+        return Math.tanh(x);
+    }
+
     // Función principal
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
                 //Clase Console para recibir entradas
                 Console console = System.console();
@@ -50,6 +84,18 @@ public class Calculadora {
                 System.out.println("1. sin(x)");
                 System.out.println("2. cos(x)");
                 System.out.println("3. tan(x)");
+
+                // *Añadidas las opciones para las inversas* - Ronald M.
+
+                System.out.println("4. arcsin(x)");
+                System.out.println("5. arccos(x)");
+                System.out.println("6. arctan(x)");
+
+                // *Añadidas las opciones para las funciones hiperbólicas* - Cristopher V.
+
+                System.out.println("7. sinh(x)");
+                System.out.println("8. cosh(x)");
+                System.out.println("9. tanh(x)");
 
                 // Variables
                 int  opcion = 0;
@@ -66,8 +112,10 @@ public class Calculadora {
                     mostrarError("Valor inválido.");
                 }
 
+                // *Se incrementó el índice* - Ronald M.
+                // *Se modificaron las opciones otra vez* - Cristopher V.
                 // Segunda comprobación
-                if (opcion < 1 || opcion > 3) {
+                if (opcion < 1 || opcion > 9) {
                     mostrarError("Opción incorrecta");
                 }
 
@@ -94,6 +142,30 @@ public class Calculadora {
                         break;
                     case 3:
                         resultado = tanX(x);
+                        break;
+
+                    // *Casos para las funciones inversas* - Ronald M.
+
+                    case 4:
+                        resultado = arcsinX(x);
+                        break;
+                    case 5:
+                        resultado = arccosX(x);
+                        break;
+                    case 6:
+                        resultado = arctanX(x);
+                        break;
+
+                    // *Añadidas las operaciones hiperbólicas* - Cristopher V.
+
+                    case 7:
+                        resultado = sinhX(x);
+                        break;
+                    case 8:
+                        resultado = coshX(x);
+                        break;
+                    case 9:
+                        resultado = tanhX(x);
                         break;
                 }
 
